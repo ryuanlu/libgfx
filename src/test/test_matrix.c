5767ba823afac057e0d1643934cb46152e41b468
盧瑞元 (Ruei-Yuan Lu) <RueiYuan.Lu@gmail.com>
2013-01-17 22:49:28 +0800

    Update test program: test_matrix

--- src/test/test_matrix.c
+++ src/test/test_matrix.c
@@ -5,14 +5,14 @@
 int main(int argc, char **argv)
 {
 	int i;
-	mat4 m = {};
+	mat4 m, n;
 
-	gfx_mat4_set_identity(m);
+	gfx_mat4_set_translate(m, 3, 4, 5);
+	gfx_mat4_set_rotate(n, 3, 4, 5, 67);
+	gfx_mat4_multiply(m, m, n);
 
 	for(i = 0; i < 16; ++i)
 		fprintf(stderr, "%f ", m[i]);
-
-
 
 	return EXIT_SUCCESS;
 }
